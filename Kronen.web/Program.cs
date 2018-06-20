969c793ad3bbda9c48d5afb866488cc7113c4596
Lucas Ribeiro <lucasgr7@gmail.com>
2018-06-19 21:08:46 -0300

    setado novo inicialização da aplicação

--- Kronen.web/Program.cs
+++ Kronen.web/Program.cs
@@ -16,14 +16,26 @@ namespace Kronen
         private static string port = "5000";
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+            // BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS"))
-                .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
-                .Build();
+        // public static IWebHost BuildWebHost(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseStartup<Startup>()
+        //         .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS"))
+        //         .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
+        //         .Build();
     }
 }
